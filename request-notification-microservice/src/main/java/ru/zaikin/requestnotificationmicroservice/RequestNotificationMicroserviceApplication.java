861f0927f5225ebd820f0138f4a65ef9e1160b84
hitoricamiter <koshurnikovaslyam@mail.ru>
2025-05-29 01:41:02 +0600

    add eureka-service, request-notification microservice module

---
+++ request-notification-microservice/src/main/java/ru/zaikin/requestnotificationmicroservice/RequestNotificationMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.zaikin.requestnotificationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestNotificationMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestNotificationMicroserviceApplication.class, args);
+    }
+
+}
---
+++ request-notification-microservice/src/test/java/ru/zaikin/requestnotificationmicroservice/RequestNotificationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zaikin.requestnotificationmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestNotificationMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
